1db45cb5f304ca1c40fad40e628e4b459961f43b
YashChavda777 <140133564+YashChavda777@users.noreply.github.com>
2023-09-03 15:39:52 +0530

    Update tb_mux.v

--- tb_mux.v
+++ tb_mux.v
@@ -1,20 +1,55 @@
-module tb_mux;
+module testbench_mux4to1;
 
-        reg i0,i1,i2,i3;
-        reg s0,s1;
-        wire y;
+    reg a, b, c, d, s1, s2;
+    wire y;
+
+  
+    mux4to1 uut (
+        .a(a),
+        .b(b),
+        .c(c),
+        .d(d),
+        .s1(s1),
+        .s2(s2),
+        .y(y)
+    );
+
+  
+    initial begin
+     
+        a = 1'b1;
+        b = 1'b0;
+        c = 1'b0;
+        d = 1'b0;
+        s1 = 1'b0;
+        s2 = 1'b0;
+        #10;  
+      
+     
+        a = 1'b0;
+        b = 1'b1;
+        c = 1'b0;
+        d = 1'b0;
+        s1 = 1'b0;
+        s2 = 1'b1;
+        #10;
+       
+       
+        a = 1'b0;
+        b = 1'b0;
+        c = 1'b1;
+        d = 1'b0;
+        s1 = 1'b1;
+        s2 = 1'b0;
+        #10;
         
-        mux_4_1 M1(.i0(i0),.i1(i1),.i2(i2),.i3(i3),.s0(s0),.s1(s1),.y(y));
-        
-            initial begin
-            i0 = 1; i1 = 0; i2 = 1; i3 = 0;
-            s1 = 1'b0; s0 = 1'b0;
-            #100;
-            s1 = 1'b0; s0 = 1'b1;
-            #100;
-            s1 = 1'b1; s0 = 1'b0;
-            #100;
-            s1 = 1'b1; s0 = 1'b1;
-            
-            end
-endmodule
+      
+        a = 1'b0;
+        b = 1'b0;
+        c = 1'b0;
+        d = 1'b1;
+        s1 = 1'b1;
+        s2 = 1'b1;
+        #10;
+             $finish;
+    end
